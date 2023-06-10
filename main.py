8498afe1259cc295cf8fa1d37d783f67781bd5ac
“Renan <renan.fmarins@gmail.com>
2023-06-10 16:58:18 -0300

    menu de opção

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 while True:
     print("Menu:")
-    print("1- Opção 1")
-    print("2- Opção 2")
-    print("3- Opção 3")
-    print("4- Sair")
+    print("1. Opção 1")
+    print("2. Opção 2")
+    print("3. Opção 3")
+    print("4. Sair")
     
     opcao = input("Digite o número da opção desejada: ")
     
     if opcao == "1":
         print("Você escolheu a opção 1")
+        # Coloque o código que corresponde à opção 1 aqui
     elif opcao == "2":
         print("Você escolheu a opção 2")
+        # Coloque o código que corresponde à opção 2 aqui
     elif opcao == "3":
         print("Você escolheu a opção 3")
+        # Coloque o código que corresponde à opção 3 aqui
     elif opcao == "4":
         print("Saindo do programa...")
         break
