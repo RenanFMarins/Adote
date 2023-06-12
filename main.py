be175f6da653309cf4c723ada70e103498d0b469
“Renan <renan.fmarins@gmail.com>
2023-06-11 21:41:18 -0300

    opção cadastrar Animal

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 while True:
     print("Menu:")
-    print("1. Opção 1")
+    print("1. Cadastrar Animal")
     print("2. Opção 2")
     print("3. Opção 3")
     print("4. Sair")
-    
     opcao = input("Digite o número da opção desejada: ")
-    
     if opcao == "1":
-        print("Você escolheu a opção 1")
-        # Coloque o código que corresponde à opção 1 aqui
+        print("Você escolheu a opção Cadastrar Animal")
     elif opcao == "2":
         print("Você escolheu a opção 2")
-        # Coloque o código que corresponde à opção 2 aqui
     elif opcao == "3":
         print("Você escolheu a opção 3")
-        # Coloque o código que corresponde à opção 3 aqui
     elif opcao == "4":
         print("Saindo do programa...")
         break
