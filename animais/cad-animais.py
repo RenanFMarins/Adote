36e5bb952793ef9cad0eb3f62614b7aba164e099
RenanFMarins <105497014+RenanFMarins@users.noreply.github.com>
2023-06-15 16:11:16 -0300

    ordenar animais por idade

--- animais/cad-animais.py
+++ animais/cad-animais.py
@@ -76,3 +76,29 @@ class Animais:
 
         escolha = input("Digite o número do tipo desejado ou 0 para adicionar um novo tipo: ")
         escolha = int(escolha)
+        
+        if escolha == 0:
+            novo_tipo = input("Digite o nome do novo tipo de animal: ")
+
+            if novo_tipo in self.animais:
+                print(f"O tipo de animal '{novo_tipo}' já existe.")
+            else:
+                self.animais[novo_tipo] = []
+                print(f"Tipo de animal '{novo_tipo}' adicionado com sucesso!")
+        elif 1 <= escolha <= len(tipos):
+            tipo_escolhido = tipos[escolha - 1]
+            animais_tipo_escolhido = self.animais[tipo_escolhido]
+
+            animais_tipo_escolhido_ordenados = sorted(animais_tipo_escolhido, key=lambda animal: int(animal['idade_aproximada']))
+
+            print(f"Animais do tipo '{tipo_escolhido}':")
+            print("--------------------------------------------------------------")
+            for animal in animais_tipo_escolhido_ordenados:
+                print("Nome:", animal['nome'])
+                print("Idade Aproximada:", animal['idade_aproximada'])
+                print("Cor:", animal['cor'])
+                print("Porte:", animal['porte'])
+                print("Particularidade:", animal['particularidade'])
+            print("--------------------------------------------------------------")
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
