98a42eeecf692c6508378db6e30d42e7c9f1b3a1
KauaArruda <117871994+KauaArruda@users.noreply.github.com>
2023-06-15 00:03:03 -0300

    Ajustes - Cadastrar / Listar Animais

--- animais/cad-animais.py
+++ animais/cad-animais.py
@@ -2,16 +2,29 @@ class Animais:
     def __init__(self):
         self.animais = {}
     
-    def cadastrar_animal(self, tipo, nome):
+    def cadastrar_animal(self, tipo, nome, idade_aproximada, cor, porte, particularidade):
+        animal = {
+            'nome': nome,
+            'idade_aproximada': idade_aproximada,
+            'cor': cor,
+            'porte': porte,
+            'particularidade': particularidade
+        }
+        
         if tipo in self.animais:
-            self.animais[tipo].append(nome)
+             self.animais[tipo].append(animal)
         else:
-            self.animais[tipo] = [nome]
+            self.animais[tipo] = [animal]
         print("Animal cadastrado com sucesso!")
         
     def listar_animais(self):
         for tipo, lista_animais in self.animais.items():
             print(f"Tipo: {tipo}")
-            print("Animais: ", end="")
-            print(*lista_animais, sep=", ")
+            
+        for animal in lista_animais:
+            print("Nome:", animal['nome'])
+            print("Idade Aproximada:", animal['idade_aproximada'])
+            print("Cor:", animal['cor'])
+            print("Porte:", animal['porte'])
+            print("Particularidade:", animal['particularidade'])
             print()
