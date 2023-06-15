8aacc374a5bc804fbaa0de94ee234d8d7e474d81
RenanFMarins <105497014+RenanFMarins@users.noreply.github.com>
2023-06-15 16:14:46 -0300

    ajuste front

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ while True:
     print("4. Sair")
     opcao = input("Digite o número da opção desejada: ")
     if opcao == "1":
-        print("Você escolheu a opção Cadastrar Animal")
+        print("--------------------------------------------------------------")
+        print("Cadastrar Animal")
+        print("--------------------------------------------------------------")
         
         tipo = input("Digite o tipo do animal: ")
         nome = input("Digite o nome do animal: ")
@@ -30,7 +32,9 @@ while True:
         cadastro.listar_tipos_animais_ordenar()
         
     elif opcao == "3":
-        print("Você escolheu a opção Cadastrar Pessoa para Adoção")
+        print("--------------------------------------------------------------")
+        print("Cadastrar Pessoa para Adoção")
+        print("--------------------------------------------------------------")
         nome = input("Digite o nome da pessoa: ")
         contato = input("Digite o contato da pessoa: ")
         tipo_preferido = input("Digite o tipo de animal preferido da pessoa: ")
@@ -42,7 +46,9 @@ while True:
         cadastro.listar_tipos_animais()
 
     elif opcao == "4":
+        print("--------------------------------------------------------------")
         print("Saindo do programa...")
+        print("--------------------------------------------------------------")
         break
         
     else:
